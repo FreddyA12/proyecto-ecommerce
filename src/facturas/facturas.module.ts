3829cd748e159e354bf010560dc0ec281df7103b
melanieAlban <malban5548@uta.edu.ec>
2024-06-02 20:06:10 -0500

    se modifico el module

--- src/facturas/facturas.module.ts
+++ src/facturas/facturas.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FacturasService } from './facturas.service';
+import { FacturasController } from './facturas.controller';
 
 @Module({
-  providers: [FacturasService]
+  providers: [FacturasService],
+  controllers: [FacturasController]
 })
 export class FacturasModule {}

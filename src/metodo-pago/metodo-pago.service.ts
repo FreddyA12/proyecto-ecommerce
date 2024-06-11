4729712ba091214175113b6af9461117b67d3848
melanieAlban <malban5548@uta.edu.ec>
2024-06-11 12:59:59 -0500

    se implemento metodos postt,get,delete,put

--- src/metodo-pago/metodo-pago.service.ts
+++ src/metodo-pago/metodo-pago.service.ts
@@ -17,4 +17,39 @@ export class MetodoPagoService {
         const metodoPago = this.metodoRepository.create(dtoMetodoPago);
         return await this.metodoRepository.save(metodoPago);
     }
+
+    async findAll(): Promise<MetodoPago[]> {
+        return await this.metodoRepository.find();
+    }
+
+    async findOne(id_metodo_pago: number): Promise<MetodoPago> {
+        const metodoPago = await this.metodoRepository.findOne({ where: { id_metodo_pago } });
+        if (!metodoPago) {
+            throw new Error('MetodoPago not found');
+        }
+        return metodoPago;
+    }
+    
+    async update(id_metodo_pago: number, dtoMetodoPago: DtoMetodoPago): Promise<MetodoPago> {
+        // Find the existing entity
+        const metodoPago = await this.metodoRepository.findOne({ where: { id_metodo_pago } });
+        if (!metodoPago) {
+            throw new Error('MetodoPago not found');
+        }
+
+        // Update the entity with new values
+        metodoPago.nombre = dtoMetodoPago.nombre;
+
+        // Save the updated entity
+        return await this.metodoRepository.save(metodoPago);
+    }
+
+    async delete(id_metodo_pago: number): Promise<void>{
+        const metodoPago = await this.metodoRepository.findOne({ where: { id_metodo_pago } });
+        if (!metodoPago) {
+            throw new Error('MetodoPago not found');
+        }
+
+        await this.metodoRepository.remove(metodoPago);
+    }
 }

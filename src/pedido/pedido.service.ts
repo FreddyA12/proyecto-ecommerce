53147fea67f6b1a1497bb5648f8b58054553bef6
melanieAlban <malban5548@uta.edu.ec>
2024-06-13 23:18:28 -0500

    ajustes modulos

--- src/pedido/pedido.service.ts
+++ src/pedido/pedido.service.ts
@@ -3,23 +3,44 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Pedido } from './pedido.entity';
 import { Repository } from 'typeorm';
 import { CreatePedidoDto } from './dto/pedido.dto';
+import { Usuario } from 'src/usuario/usuario.entity';
+import { MetodoPago } from 'src/metodo-pago/metodo-pago.entity';
 
 @Injectable()
 export class PedidoService {
     constructor(
         @InjectRepository(Pedido)
         private pedidoRepository: Repository<Pedido>,
+        @InjectRepository(Usuario)
+        private usuarioRepository: Repository<Usuario>,
+        @InjectRepository(MetodoPago)
+        private metodoPagoRepository: Repository<MetodoPago>
       ) {}
 
     // Crear una nueva factura
     async create(createPedidoDto: CreatePedidoDto): Promise<Pedido> {
-        const pedido = new Pedido();
-        // Asegúrate de que la fecha sea un objeto Date
-        pedido.fecha = new Date(createPedidoDto.fecha);
-        pedido.total = createPedidoDto.total;
-
-        return await this.pedidoRepository.save(pedido);
-    }
+      const pedido = new Pedido();
+      pedido.fecha = new Date(createPedidoDto.fecha);
+      pedido.total = createPedidoDto.total;
+      pedido.estado = createPedidoDto.estado;
+      pedido.subtotal = createPedidoDto.subtotal;
+  
+      // Obtener el objeto usuario utilizando el ID
+      const usuario = await this.usuarioRepository.findOneBy({ id_usuario: createPedidoDto.id_usuario });
+      if (!usuario) {
+          throw new Error('Usuario no encontrado');
+      }
+      pedido.usuario = usuario;
+  
+      // Asumiendo que tienes un repositorio para Método de Pago similar al de Usuario
+      const metodoPago = await this.metodoPagoRepository.findOneBy({ id_metodo_pago: createPedidoDto.id_metodo_pago });
+      if (!metodoPago) {
+          throw new Error('Método de pago no encontrado');
+      }
+      pedido.metodoPago = metodoPago;
+  
+      return await this.pedidoRepository.save(pedido);
+  }
     
 
   // Encontrar todas las facturas

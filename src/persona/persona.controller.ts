af2b50f2af9dfbad5e3eb9fdd27eec15b18bdecf
FreddyA12 <2balvarezfreddy@gmail.com>
2024-06-13 23:18:45 -0500

    Actualizacion de persona y creacion de tipo de identifiacion

--- src/persona/persona.controller.ts
+++ src/persona/persona.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+// persona.controller.ts
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { PersonaService } from './persona.service';
+import { Persona } from './persona.entity';
 
-@Controller('persona')
-export class PersonaController {}
+@Controller('personas')
+export class PersonaController {
+    constructor(private readonly personaService: PersonaService) {}
+
+    @Get()
+    findAll(): Promise<Persona[]> {
+        return this.personaService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number): Promise<Persona> {
+        return this.personaService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() persona: Persona): Promise<Persona> {
+        return this.personaService.create(persona);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() persona: Partial<Persona>): Promise<Persona> {
+        return this.personaService.update(id, persona);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: number): Promise<void> {
+        return this.personaService.remove(id);
+    }
+}
